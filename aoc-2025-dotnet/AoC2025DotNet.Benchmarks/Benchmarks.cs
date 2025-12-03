6982803a8e1613713c7fe043948f56f92c0828a4
Josh Montgomery <josh.montgomery@autorek.com>
2025-12-03 11:15:22 +0000

    feat: add initial benchmarks

---
+++ aoc-2025-dotnet/AoC2025DotNet.Benchmarks/Benchmarks.cs
@@ -0,0 +1,45 @@
+﻿using AoC2025DotNet.Benchmarks.Dependencies;
+using AoC2025DotNet.Helpers;
+using AoC2025DotNet.SolutionFinding;
+using AoC2025DotNet.SolutionFinding.Solutions;
+using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+
+namespace AoC2025DotNet.Benchmarks
+{
+    // For more information on the VS BenchmarkDotNet Diagnosers see https://learn.microsoft.com/visualstudio/profiling/profiling-with-benchmark-dotnet
+    // [CPUUsageDiagnoser]
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        //public IEnumerable<ISolution> Solutions()
+        //{
+        //    IConsoleWriter consoleWriter = new NullConsoleWriter();
+
+        //    yield return new Day01(consoleWriter);
+        //    yield return new Day02(consoleWriter);
+        //}
+
+        //[Benchmark]
+        //[ArgumentsSource(nameof(Solutions))]
+        //public void RunSolutions(ISolution solution)
+        //{
+        //    solution.Solve(0);
+        //}
+
+        private static readonly IConsoleWriter s_consoleWriter = new NullConsoleWriter();
+        private readonly ISolution _solution = new Day02(s_consoleWriter);
+
+        [Benchmark]
+        public void SolutionPartOne()
+        {
+            _solution.Solve(1);
+        }
+
+        [Benchmark]
+        public void SolutionPartTwo()
+        {
+            _solution.Solve(2);
+        }
+    }
+}
---
+++ aoc-2025-dotnet/AoC2025DotNet.Benchmarks/Dependencies/NullConsoleWriter.cs
@@ -0,0 +1,22 @@
+﻿using AoC2025DotNet.Helpers;
+
+namespace AoC2025DotNet.Benchmarks.Dependencies
+{
+    internal class NullConsoleWriter : IConsoleWriter
+    {
+        public void WriteDebug(string message)
+        {
+            // Do nothing
+        }
+
+        public void WriteError(string message)
+        {
+            // Do nothing
+        }
+
+        public void WriteInfo(string message)
+        {
+            // Do nothing
+        }
+    }
+}
